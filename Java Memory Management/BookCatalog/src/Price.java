c9e63d56677d6a64a82334177970eff22073ef09
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-07-24 15:07:33 +0300

    Java Memory Management

---
+++ Java Memory Management/BookCatalog/src/Price.java
@@ -0,0 +1,37 @@
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Price {
+
+    private Map<String, Double> rates;
+    private Double value;
+
+    public Price(Double value) {
+        this.value = value;
+        rates = new HashMap<>();
+        rates.put("USD", 1d);
+        rates.put("GBP", 0.6);
+        rates.put("EUR", 0.8);
+    }
+
+    public Double convert(String toCurrency) {
+
+        if (toCurrency.equals("USD")) {
+            return value;
+        } else {
+            Double conversion = rates.get("USD") / rates.get(toCurrency);
+            return conversion * value;
+        }
+    }
+
+    public String toString() {
+        return this.value.toString();
+    }
+
+    public Map<String, Double> getRates() {
+        return Collections.unmodifiableMap(rates);
+    }
+
+}
